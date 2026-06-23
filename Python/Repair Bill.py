27c3c964d8fc05a1d63cf3514dbd97f1a52385a0
Ahamed <ahamedponmala@gmail.com>
2026-06-23 13:17:47 +0530

    bill

---
+++ Python/Repair Bill.py
@@ -0,0 +1,15 @@
+print("customer details")
+print("------------------")
+brand = input("Enter brand\n")
+model = input("Enter model\n")
+complaint = input("Enter complaint\n")
+imei = input("Enter IMEI number\n")
+repair_cost = float(input("Enter repair cost\n"))
+discount = repair_cost * 10 / 100
+final_bill = repair_cost - discount
+print ("::::Brite Shoppe::::")
+print("Brand: ", brand)
+print("Model: ", model)
+print("Repair Cost: ", repair_cost)
+print("Discount 10%: ", discount)
+print("Final Bill Amount: ", final_bill)
