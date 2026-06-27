7f4afb10725638d951c5ef826844cfbcbe28926e
Ahamed <ahamedponmala@gmail.com>
2026-06-27 10:03:39 +0530

    Added line name print to Day 2 task 2

---
+++ Python/voltage test (if elif else).py
@@ -0,0 +1,9 @@
+line_name = input("Enter Power Line Name: \n")
+voltage = float(input("Enter Measured Voltage: \n"))
+print(f"\nLine: {line_name}")
+if voltage == 0:
+    print("Result: Short Circuit or Dead Line!")
+elif voltage > 4.2:
+    print("Result: High Voltage! Risk of Component Damage!")
+else:
+    print("Result: Normal Operating Voltage.")
