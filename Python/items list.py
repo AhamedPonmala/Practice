005cbe90acb94aac35991137a071468a9d722cd7
Ahamed <ahamedponmala@gmail.com>
2026-07-02 11:00:02 +0530

    List and Dictionaries

---
+++ Python/items list.py
@@ -0,0 +1,6 @@
+tools = ["Multimeter", "DSO", "DC Power Supply"]
+repair_job = {"customer" : "Ahamed",
+              "complaint" : "NAND Error",
+              "cost" : 3500}
+print( tools [1])
+print( repair_job["customer"] )
